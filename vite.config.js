778c5df7c36979016e31e07c3c004a9b4fa7d2d8
muzamalmahmood <muzamal.mahmood@purelogics.net>
2026-04-07 19:58:02 +0500

    First

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [
+    react(),
+    eslint({
+      include: ['src/**/*.js', 'src/**/*.jsx'],
+      exclude: ['src/test/**'], // ✅ prevent ESLint from touching tests
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
